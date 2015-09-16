2f730c9e073952b807d81ce86b6e350eec7c5e28
Breezess <s88858@163.com>
2015-09-16 18:16:51 +0800

    代码优化

--- app/src/main/java/com/example/breeze/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/breeze/myapplication/LoginActivity.java
@@ -331,17 +331,4 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    private String getAppInfo() {
-        try {
-            String pkName = this.getPackageName();
-            String versionName = this.getPackageManager().getPackageInfo(
-                    pkName, 0).versionName;
-            int versionCode = this.getPackageManager()
-                    .getPackageInfo(pkName, 0).versionCode;
-            return pkName + "   " + versionName + "  " + versionCode;
-        } catch (Exception e) {
-        }
-        return null;
-    }
-
 }
